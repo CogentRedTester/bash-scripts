84081d75c06d2f8c528938f3f6f090b7ff5a5a00
CogentRedTester <cogent.redtester@outlook.com>
2021-03-09 23:34:38 +1030

    switched to using array syntax to properly pass filenames to programs

--- audio_encode.sh
+++ audio_encode.sh
@@ -5,8 +5,9 @@
 # new stream is a copy of the given audio track number reencoded in ac3
 #
 
-file=$( printf "%q" "$1" )
-file="$1"
+file=()
+file[0]=( "$(printf "%q" "$1")" )
+file[0]="$1"
 main_stream=$2
 
 if [ -z $main_stream ]
@@ -16,11 +17,11 @@ else
 	main_stream=$(($2 - 1))
 fi
 
-num_audio=$(ffprobe -v error -show_entries stream=codec_type,index "$file" -print_format compact | grep audio -c)
+num_audio=$(ffprobe -v error -show_entries stream=codec_type,index "${file[0]}" -print_format compact | grep audio -c)
 
 if (( $num_audio < 1 ))
 then
-	return
+	echo "could not detect audio tracks from "${file[0]}""
 	exit
 fi
 
@@ -34,15 +35,13 @@ do
 	args+=("copy")
 done
 
-if ffmpeg -i "$file" -c copy -map 0 -map -0:a -map 0:a:$main_stream -c:a:0 ac3 -b:a:0 640k ${args[@]} out.mkv
+if ffmpeg -i "${file[0]}" -c copy -map 0 -map -0:a -map 0:a:$main_stream -c:a:0 ac3 -b:a:0 640k ${args[@]} out.mkv
 then
 	echo "success"
-	mv $file $( printf "%q" "${1}.original" )
-	mv out.mkv $file
+	file[1]=${file[0]}.original
+	mv "${file[0]}" "${file[1]}"
+	mv out.mkv "${file[0]}"
 else
 	echo "conversion did not exit successfully"
 	exit
 fi
-
-mv $file $( printf "%q" "${1}.original" )
-mv out.mkv $file
